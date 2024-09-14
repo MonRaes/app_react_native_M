c0822863c3d8f147473dd035298d3146997ff333
Mon_Raes <130837443+MonRaes@users.noreply.github.com>
2024-09-14 00:56:45 +0000

    13.09

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button } from "react-native";
 import ListaProdutos from "./Componentes/Adaptadores/ListaProdutos";
 import axios from 'axios'
 import { useEffect } from "react";
-
-
-
+import { useRouter } from "expo-router";
 
 export default function Index() {
-  let [contador,setContador]= useState(0);
-  let [produtos,setProdutos]= useState([]);
+  const router = useRouter();
+  let [produtos, setProdutos] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     carregaProdutos();
-  },[])
-  
+  }, [])
 
-  function carregaProdutos(){
+
+  function carregaProdutos() {
     axios.get('https://api-docker-2t8m.onrender.com/api/produtos')
-      .then((resp)=>{
+      .then((resp) => {
         setProdutos(resp.data);
       })
 
@@ -28,39 +26,34 @@ export default function Index() {
     <View
       style={estilo.container}
     >
-     <ListaProdutos produtos={produtos}></ListaProdutos>
-     <Button title={contador.toString()}
-      onPress={()=>{clicarBotao()}} >
-     </Button>
+      <ListaProdutos produtos={produtos}></ListaProdutos>
+      <Button title={"cadastrar"}
+        onPress={() => { Tela_cadastro() }}></Button>
 
-     <CadastroProduto/>
-
-      
     </View>
   );
 
-  function clicarBotao(){
-    setContador(contador+1);
-  
+  function Tela_cadastro() {
+    router.push('/cadastro');
   }
 }
 
 
 
-const estilo= StyleSheet.create({
+const estilo = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "flex-start",
     backgroundColor: "#f5f5f5",
-    paddingStart:20,
-    
+    paddingStart: 20,
+
   },
   text: {
     color: "#FFFFFF",
     fontSize: 20,
   },
-  titulo:{
+  titulo: {
     color: "#000000",
     fontSize: 30,
     textAlign: "left",
